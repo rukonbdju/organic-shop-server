cf558d8b11555f0a1ea74d8ac612f36ee135355f
rukonbd1 <rukonbd.dev@gmail.com>
2023-08-10 00:09:59 +0600

    controller and router setup

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const port = process.env.PORT || 3000;
 app.use(express.json())
 app.use(cors())
 
-
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/products", productRouter)
 app.use("/api/v1/orders", orderRouter)
